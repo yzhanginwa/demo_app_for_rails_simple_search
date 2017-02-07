1fad9764f38cb40c50b2f2a6689f83a6ad41cceb
ethan <yzhang.wa@gmail.com>
2017-02-07 21:26:18 +0800

    to add column age onto table users

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231065104) do
+ActiveRecord::Schema.define(version: 20170207132430) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address1"
@@ -46,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161231065104) do
     t.integer  "address_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
